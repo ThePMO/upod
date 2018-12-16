05f997e354f6e2293fa48a292571d056f9b778e8
Boris Wachtmeister <boris-code@gmx.com>
2018-12-16 16:41:42 +0100

    fixed synchronized

--- app/src/mobi/upod/util/Hashing.scala
+++ app/src/mobi/upod/util/Hashing.scala
@@ -9,7 +9,7 @@ object Hashing {
   private val digest = MessageDigest.getInstance("SHA-256")
 
   def sha256(str: String): String = {
-    val hash = synchronized(digest) {
+    val hash = digest.synchronized {
       digest.digest(str.getBytes(Charset.utf8))
     }
     new String(Base64.encode(hash, Base64.NO_WRAP))
